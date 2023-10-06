89619f4486680e8c8e9777435f8b1e5b3ce89ca1
LanVnR <146451866+LanVnR@users.noreply.github.com>
2023-10-06 16:35:56 +0200

    Add files via upload

---
+++ code/proof of concepts/interrupt/startstopinterrupt.ino
@@ -0,0 +1,24 @@
+const int Button = 2;
+const int ledPin = 12;
+bool Running;
+
+void setup() {
+  analogReference(DEFAULT);
+  pinMode(Button, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(Button), Interrupt, RISING);
+}
+
+void loop() {
+  if (Running == true) {
+    digitalWrite(ledPin, HIGH);
+   
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+void Interrupt() {
+  Running = ! Running;
+}
